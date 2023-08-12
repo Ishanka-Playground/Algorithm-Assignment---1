1bb89fa8db5f6aac7a60e2decb48e5a06a5e74be
Ishanka Dilshan <ishankadilshan64@gmail.com>
2023-08-13 01:14:42 +0530

    Algo-3

---
+++ Algo3.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class Algo3 {
+    public static void main(String[] args) {
+
+        int[] nums = { 10, 20, 30, 40, 50 };
+        for (int i = 0; i < nums.length / 2; i++) {
+            int value = nums[i];
+            nums[i] = nums[nums.length - 1 - i];
+            nums[nums.length - 1 - i] = value;
+          
+
+        }
+        String arrayString = Arrays.toString(nums);
+        System.out.println(arrayString);
+
+    }
+
+}
